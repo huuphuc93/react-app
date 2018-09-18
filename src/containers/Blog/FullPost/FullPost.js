2d118a29a0b3ac553d828b33ab3e6f9fd48f136d
mai.huu.phuc <FRAMGIA\mai.huu.phuc@framgia0377-lt.framgia.com>
2018-09-18 17:53:14 +0700

    init project

---
+++ src/containers/Blog/FullPost/FullPost.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './FullPost.css';
+
+class FullPost extends Component {
+    state = {
+        loaderPost: null
+    }
+    componentDidMount() {
+        console.log(this.props)
+        if(this.props.match.params.id) {
+            if(!this.state.loaderPost || (this.state.loaderPost && this.state.loaderPost.id !== this.props.id)) {
+                axios.get('/posts/' + this.props.match.params.id)
+                    .then(respone => {
+                        this.setState({loaderPost: respone.data});
+                        // console.log(respone);
+                    });
+            }
+        }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('/posts/' + this.props.match.params.id)
+            .then(respone => {
+                console.log(respone);
+            });
+    }
+
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if(this.props.match.params.id) {
+            post = <p style={{textAlign: 'center'}}>Loading...!</p>;
+        }
+        if(this.state.loaderPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loaderPost.title}</h1>
+                    <p>{this.state.loaderPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
+                    </div>
+                </div>
+
+            );
+        }
+        return post;
+    }
+}
+
+export default FullPost;

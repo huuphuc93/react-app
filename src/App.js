7b600a9394abf4bce845d92a7d667597a5738f24
mai.huu.phuc <FRAMGIA\mai.huu.phuc@framgia0377-lt.framgia.com>
2018-09-20 16:17:57 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename="/my-ap">
       <BrowserRouter>
         <div className="App">
           <Blog />
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
@@ -33,7 +38,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" exact component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" exact component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     {/* <Redirect from="/" to="/posts" /> */}
                     <Route render={() => <h1>Not found</h1>} />
